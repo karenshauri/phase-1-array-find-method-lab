e2069bd2873ccc4a24dc59c18444d639a0c4c1d1
karenshauri <karennduta12@gmail.com>
2025-06-29 18:14:46 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+    
+  const winningYear = record.find(game => game.result === "W");
+  
+  return winningYear ? winningYear.year : undefined;
+}
